10d114e6fc8d241c1d7696da64d5fe9f018f5ade
游承翰 <chenghan03131@gapp.nthu.edu.tw>
2021-05-12 09:12:48 +0800

    Post .gitignore change

---
+++ simple_np_cc2640r2lp_stack_library/FlashROM_Library/ICallBLE/ble_dispatch_lite.d
@@ -0,0 +1,121 @@
+# FIXED
+
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/icall/stack/ble_dispatch_lite.c
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/osal/src/inc/osal_snv.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/hal/src/target/_common/hal_types.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/hal/src/target/_common/../_common/cc26xx/_hal_types.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdint.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdbool.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/osal/src/inc/osal_bufmgr.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/icall/inc/ble_dispatch.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/icall/inc/ble_dispatch_lite.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/inc/sm.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/inc/bcomdef.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/osal/src/inc/comdef.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/hal/src/target/_common/hal_types.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/hal/src/inc/hal_defs.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/osal/src/inc/osal.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/limits.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/osal/src/inc/osal_memory.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/osal/src/inc/osal_timers.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/icall/src/inc/icall.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdlib.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/linkage.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/hal/src/inc/hal_assert.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/hal/src/target/_common/hal_types.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/inc/hci.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/controller/cc26xx_r2/inc/ll_common.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/drivers/rf/RF.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/drivers/dpl/ClockP.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stddef.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/drivers/dpl/SemaphoreP.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stddef.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/devices/DeviceFamily.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/devices/cc26x0r2/driverlib/rf_common_cmd.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/devices/cc26x0r2/driverlib/rf_mailbox.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/string.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/devices/cc26x0r2/driverlib/rf_prop_cmd.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/devices/cc26x0r2/driverlib/rf_ble_cmd.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/hal/src/target/_common/cc26xx/rf_hal.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/hal/src/target/_common/hal_types.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/controller/cc26xx_r2/inc/ll.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/controller/cc26xx_r2/inc/ll_scheduler.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/inc/gap.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/inc/gatt.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/inc/att.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/inc/l2cap.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/inc/gatt_uuid.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/inc/gattservapp.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/profiles/roles/gapbondmgr.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/icall/inc/icall_ble_apimsg.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/inc/hci_ext.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/inc/hci_tl.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/inc/hci_data.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/inc/hci_event.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/inc/hci_tl.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/inc/gapgattserver.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/inc/linkdb.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/inc/gatt_profile_uuid.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/inc/../rom/rom_jt.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/inc/../rom/map_direct.h
+ICallBLE/ble_dispatch_lite.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/icall/src/inc/icall_lite_translation.h
+
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/icall/stack/ble_dispatch_lite.c: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/osal/src/inc/osal_snv.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/hal/src/target/_common/hal_types.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/hal/src/target/_common/../_common/cc26xx/_hal_types.h: 
+D:/ProgramFiles/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdint.h: 
+D:/ProgramFiles/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdbool.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/osal/src/inc/osal_bufmgr.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/icall/inc/ble_dispatch.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/icall/inc/ble_dispatch_lite.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/inc/sm.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/inc/bcomdef.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/osal/src/inc/comdef.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/hal/src/target/_common/hal_types.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/hal/src/inc/hal_defs.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/osal/src/inc/osal.h: 
+D:/ProgramFiles/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/limits.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/osal/src/inc/osal_memory.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/osal/src/inc/osal_timers.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/icall/src/inc/icall.h: 
+D:/ProgramFiles/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdlib.h: 
+D:/ProgramFiles/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/linkage.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/hal/src/inc/hal_assert.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/hal/src/target/_common/hal_types.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/inc/hci.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/controller/cc26xx_r2/inc/ll_common.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/drivers/rf/RF.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/drivers/dpl/ClockP.h: 
+D:/ProgramFiles/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stddef.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/drivers/dpl/SemaphoreP.h: 
+D:/ProgramFiles/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stddef.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/devices/DeviceFamily.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/devices/cc26x0r2/driverlib/rf_common_cmd.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/devices/cc26x0r2/driverlib/rf_mailbox.h: 
+D:/ProgramFiles/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/string.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/devices/cc26x0r2/driverlib/rf_prop_cmd.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/devices/cc26x0r2/driverlib/rf_ble_cmd.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/hal/src/target/_common/cc26xx/rf_hal.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/hal/src/target/_common/hal_types.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/controller/cc26xx_r2/inc/ll.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/controller/cc26xx_r2/inc/ll_scheduler.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/inc/gap.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/inc/gatt.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/inc/att.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/inc/l2cap.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/inc/gatt_uuid.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/inc/gattservapp.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/profiles/roles/gapbondmgr.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/icall/inc/icall_ble_apimsg.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/inc/hci_ext.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/inc/hci_tl.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/inc/hci_data.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/inc/hci_event.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/inc/hci_tl.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/inc/gapgattserver.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/inc/linkdb.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/inc/gatt_profile_uuid.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/inc/../rom/rom_jt.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/inc/../rom/map_direct.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/icall/src/inc/icall_lite_translation.h: 
---
+++ simple_np_cc2640r2lp_stack_library/FlashROM_Library/ICallBLE/icall_lite_translation.d
@@ -0,0 +1,37 @@
+# FIXED
+
+ICallBLE/icall_lite_translation.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/icall/src/icall_lite_translation.c
+ICallBLE/icall_lite_translation.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/osal/src/inc/osal.h
+ICallBLE/icall_lite_translation.obj: D:/ProgramFiles/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/limits.h
+ICallBLE/icall_lite_translation.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/osal/src/inc/comdef.h
+ICallBLE/icall_lite_translation.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/hal/src/target/_common/hal_types.h
+ICallBLE/icall_lite_translation.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/hal/src/target/_common/../_common/cc26xx/_hal_types.h
+ICallBLE/icall_lite_translation.obj: D:/ProgramFiles/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdint.h
+ICallBLE/icall_lite_translation.obj: D:/ProgramFiles/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdbool.h
+ICallBLE/icall_lite_translation.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/hal/src/inc/hal_defs.h
+ICallBLE/icall_lite_translation.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/osal/src/inc/osal_memory.h
+ICallBLE/icall_lite_translation.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/osal/src/inc/osal_timers.h
+ICallBLE/icall_lite_translation.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/icall/src/inc/icall.h
+ICallBLE/icall_lite_translation.obj: D:/ProgramFiles/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdlib.h
+ICallBLE/icall_lite_translation.obj: D:/ProgramFiles/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/linkage.h
+ICallBLE/icall_lite_translation.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/hal/src/inc/hal_assert.h
+ICallBLE/icall_lite_translation.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/hal/src/target/_common/hal_types.h
+ICallBLE/icall_lite_translation.obj: D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/icall/src/inc/icall_lite_translation.h
+
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/icall/src/icall_lite_translation.c: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/osal/src/inc/osal.h: 
+D:/ProgramFiles/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/limits.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/osal/src/inc/comdef.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/hal/src/target/_common/hal_types.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/hal/src/target/_common/../_common/cc26xx/_hal_types.h: 
+D:/ProgramFiles/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdint.h: 
+D:/ProgramFiles/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdbool.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/hal/src/inc/hal_defs.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/osal/src/inc/osal_memory.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/osal/src/inc/osal_timers.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/icall/src/inc/icall.h: 
+D:/ProgramFiles/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdlib.h: 
+D:/ProgramFiles/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/linkage.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/hal/src/inc/hal_assert.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/hal/src/target/_common/hal_types.h: 
+D:/ProgramFiles/ti/simplelink_cc2640r2_sdk_1_50_00_58/source/ti/blestack/icall/src/inc/icall_lite_translation.h: 
